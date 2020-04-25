61247945939a3ac82d9f89f6b571d3a965bf6e87
neer2808 <uic.neerajkhanna@gmail.com>
2020-04-25 12:01:34 +0530

    Add Extract method

--- src/BinaryHeapwithArrayK.java
+++ src/BinaryHeapwithArrayK.java
@@ -1,12 +1,70 @@
 public class BinaryHeapwithArrayK {
   int arr[];
   int sizeofheap;
-
   public BinaryHeapwithArrayK(int size)
   {
     arr = new int[size +1];
     sizeofheap = 0;
   }
+  public int extract()
+  {
+    if(sizeofheap ==0)
+    {
+      System.out.println("Heap is empty ");
+      return -1;
+    }
+    else
+    {
+      int result = arr[1];
+      arr[1] = arr[sizeofheap];
+      sizeofheap--;
+      heapifyextract(1);
+      return result;
+
+    }
+  }
+
+  public void heapifyextract(int index)
+  {
+    int left = index *2;
+    int right = (index*2) +1;
+    int smallest = 0;
+    if(sizeofheap<left)
+    {
+      return;
+    }
+    else if (sizeofheap==left)
+    {
+       if(arr[index]>arr[left])
+       {
+         int temp = arr[index];
+         arr[index]= arr[left];
+         arr[left]= temp;
+       }
+       return ;
+    }
+    else
+    {
+      if(arr[left]<arr[right])
+
+        smallest= left;
+      else
+        smallest= right;
+      if(arr[index]> arr[smallest])
+      {
+        int temp = arr[index];
+        arr[index]= arr[smallest];
+        arr[smallest]= temp;
+      }
+    }
+    heapifyextract(smallest);
+
+  }
+
+
+
+
+
   public  void insert(int value)
   {
     arr[sizeofheap+1]= value;
@@ -27,7 +85,37 @@ public class BinaryHeapwithArrayK {
     }
     heapify(parent);
   }
-
+  public int sizeofarray()
+  {
+    return arr.length;
+  }
+  public int getSizeofheap()
+  {
+    return sizeofheap;
+  }
+  public boolean isheapemmpty()
+  {
+    if(sizeofheap <= 0 )
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  public void deleteheap()
+  {
+    arr = null;
+    System.out.println("heap destroy");
+  }
+  public void peek() {
+    if(isheapemmpty() == true) {
+      System.out.println("empty");
+    }
+    else
+      System.out.println(arr[1]);
+  }
   // represents level order traversal
   public void display()
   {
--- src/BinaryHeapwithArrayKmain.java
+++ src/BinaryHeapwithArrayKmain.java
@@ -1,14 +1,18 @@
 public class BinaryHeapwithArrayKmain {
 
   public static void main(String[] args) {
-    BinaryHeapwithArrayK obj = new BinaryHeapwithArrayK(20);
+    BinaryHeapwithArrayK obj = new BinaryHeapwithArrayK(5);
     obj.insert(12);
     obj.insert(7);
-    obj.insert(6);
-    obj.insert(10);
-    obj.insert(8);
-    obj.insert(20);
-    obj.display();
+
+    System.out.println(obj.extract());
+//    obj.insert(6);
+//    obj.insert(10);
+//    obj.insert(8);
+//    obj.insert(20);
+//    obj.display();
+    // 6   8    7   12    10   20
+
 
   }
 }
